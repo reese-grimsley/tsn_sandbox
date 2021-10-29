3afc62e62706e5eb8b9dc223d2773a2521ed59f6
Reese <rgrimsle@andrew.cmu.edu>
2021-10-29 17:57:03 -0400

    configure source for TSN pkts

--- eth_priority_tests/constants.h
+++ eth_priority_tests/constants.h
@@ -1,13 +1,13 @@
 //Addresses.h
 
 #define JAMMER_NAME "tsn3"
-#define JAMMER_MAC_ADDR 0x54b203f0d468 //54:b2:03:f0:d4:68
+#define JAMMER_MAC_ADDR {0x54, 0xb2, 0x03, 0xf0, 0xd4, 0x68}  //54:b2:03:f0:d4:68
 #define JAMMER_IP_ADDR  "192.168.1.224"
 #define SOURCE_NAME "tsn1"
-#define SOURCE_MAC_ADDR 0x54b203f0d50b //54:b2:03:f0:d5:0b
+#define SOURCE_MAC_ADDR {0x54, 0xb2, 0x03, 0xf0, 0xd5, 0x0b} //54:b2:03:f0:d5:0b
 #define SOURCE_IP_ADDR "192.168.1.229"
 #define SINK_NAME 'tsn2'
-#define SINK_MAC_ADDR 0x54b203f0d455  //54:b2:03:f0:d4:55
+#define SINK_MAC_ADDR {0x54, 0xb2, 0x03, 0xf0, 0xd4, 0x55}  //54:b2:03:f0:d4:55
 #define SINK_IP_ADDR "192.168.1.231"
 #define SINK_PORT 15810
 
