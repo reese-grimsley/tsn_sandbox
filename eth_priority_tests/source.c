f102ff297da738981e9299cb0f9aba8c27668814
Reese <rgrimsle@andrew.cmu.edu>
2021-10-29 17:54:00 -0400

    configure source for TSN pkts

--- eth_priority_tests/source.c
+++ eth_priority_tests/source.c
@@ -64,8 +64,8 @@ int main(int argc, char* argv[])
     addr.sll_pkttype = PACKET_OTHERHOST;
 
     //send packets
-    char[ETHER_ADDR_LEN] dest_addr = SINK_MAC_ADDR;
-    char[ETHER_ADDR_LEN] src _addr = SOURCE_MAC_ADDR;
+    char dest_addr[ETHER_ADDR_LEN] = SINK_MAC_ADDR;
+    char src_addr[ETHER_ADDR_LEN] = SOURCE_MAC_ADDR;
     memcpy(&(addr.sll_addr), ETHER_ADDR_LEN, &dest_addr);
 
     // struct ether_tsn tsn_ethernet;
