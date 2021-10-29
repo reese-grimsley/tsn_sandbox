ec02e2771bc7bd5767d09f492428ea0e14b34536
Reese <rgrimsle@andrew.cmu.edu>
2021-10-29 16:39:32 -0400

    socket timestamping

---
+++ eth_priority_tests/helpers.c
@@ -0,0 +1,60 @@
+#include "helpers.h"
+
+
+int configure_hw_timestamping(int sock_fd)
+{
+    int flags;
+    struct hwtstamp_config hwts_config;
+    struct ifreq ifr;
+
+
+    flags   = SOF_TIMESTAMPING_RX_SOFTWARE
+            | SOF_TIMESTAMPING_RX_HARDWARE 
+            | SOF_TIMESTAMPING_RAW_HARDWARE;
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_TIMESTAMPING, &flags, sizeof(flags)) < 0)
+    {
+        printf("ERROR: setsockopt SO_TIMESTAMPING: [%d]\n", errno);
+        return errno;
+        }
+
+    /* Enable hardware timestamping on the interface */
+    memset(&hwts_config, 0, sizeof(hwts_config));
+    hwts_config.tx_type = HWTSTAMP_TX_ON;
+    hwts_config.rx_filter = HWTSTAMP_FILTER_ALL;
+    memset(&ifr, 0, sizeof(ifr));    
+    strncpy(ifr.ifr_name, ETH_INTERFACE_I225, sizeof(ifr.ifr_name));
+    ifr.ifr_data = (void *)&hwts_config;
+    if (ioctl(sock_fd , SIOCSHWTSTAMP, &ifr) == -1)
+    {
+        printf("failed to set hardware timestamping ioctl");
+        return -1;
+    }
+    if (ioctl(sock_fd , SIOCGHWTSTAMP, &ifr) == -1)
+    {
+        printf("failed to set hardware timestamping ioctl");
+        return -1;
+    }
+}
+
+
+int get_hw_timestamp_from_msg(struct msghdr& msg, struct timespec* ts)
+{
+    int level, type;
+    struct timespec* ts_from_msg;
+
+    int found_timespec = false;
+    for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg))
+    {
+        if (SOL_SOCKET == cmsg->cmsg_level && SO_TIMESTAMPING == cmsg->cmsg_type) {
+            ts_from_msg = (struct timespec *) CMSG_DATA(cmsg);
+            // printf("TIMESTAMP %ld.%09ld\n", (long)ts_from_msg[2].tv_sec, (long)ts_from_msg[2].tv_nsec);
+            ts->tv.sec = ts_from_msg[2].tv_sec;
+            ts->tv.nsec = ts_from_msg[2].tv_nsec;
+            found_timespec = true;
+        }
+    }
+
+    return found_timespec
+
+
+}
