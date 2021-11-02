3ae82498e4a0c2ec09170ccdabff1e73b741b82f
Reese <rgrimsle@andrew.cmu.edu>
2021-11-02 19:22:27 -0400

    debugging 802.1Q between src and sink

--- eth_priority_tests/types.h
+++ eth_priority_tests/types.h
@@ -15,7 +15,6 @@ struct ethernet_frame_8021Q
     char destination_mac[6];
     char source_mac[6];
     tag_control_t TCI; // highest 3 bits are PCP (priority), next bit is drop-elibile indicator, and last 12 are VLAN id
-    uint16_t transport_protocol; //first two bytes are 0x8100 == ETH_P_8021Q
     uint16_t data_size_or_type ; 
     char data[MAX_FRAME_DATA_LEN]; //technically, max per frame should be 1482 bytes; 18 byte header and MTU is is 1500
 };
