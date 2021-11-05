b88b0a68ab0c62470399ef7d1d435b7fcb6d37ee
Reese <rgrimsle@andrew.cmu.edu>
2021-11-05 16:18:22 -0400

    run as root

--- ptp/ptp_master/run_ptp_master.sh
+++ ptp/ptp_master/run_ptp_master.sh
@@ -4,8 +4,8 @@ if [[ `whoami` != "root" ]]; then
     echo "Run as root/sudo!"; exit -1;
 fi
 
-sudo ptp4l -f ptp_master.conf -i enp87s0 -m >> ptp.log &
+sudo ptp4l -f ptp_master.conf -i enp87s0 -m &> ptp.log &
 echo "ptp4l is running in bg as pid $!"
 
-sudo phc2sys -c enp87s0 -s CLOCK_REALTIME -w -m >> phc2sys.log & 
+sudo phc2sys -c enp87s0 -s CLOCK_REALTIME -w -m &> phc2sys.log & 
 echo "phc2sys (sync NIC clock to sys clock) is running in bg as pid $!"
