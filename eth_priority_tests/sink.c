00b295e6bc6086968a618e03ecadbac39890c3a5
Reese <rgrimsle@andrew.cmu.edu>
2021-11-05 18:34:22 -0400

    try vlan again

--- eth_priority_tests/sink.c
+++ eth_priority_tests/sink.c
@@ -238,43 +238,22 @@ void thread_recv_source_data()
             //if this came within a 802.1Q frame, the offsets will need to change to account for ethernet addresses
             int header_len = sizeof(frame) - sizeof(frame.data);
             int print_size = min(header_len, msg.msg_iov->iov_len);
-            printf("protocol: %04x\n",((struct sockaddr_ll*) msg.msg_name)->sll_protocol);//can filter based on this as well..
-            print_hex(msg.msg_iov->iov_base, print_size+20);
-            printf("\n"); 
-            printf("time since start: ");
-            time_diff(&start, &now, &diff);
-            print_timespec(diff);
-            printf("\n\n");
+            printf("receive message with protocol: %04x\n",((struct sockaddr_ll*) msg.msg_name)->sll_protocol);//can filter based on this as well..
 
             if ( (((struct sockaddr_ll*) msg.msg_name)->sll_protocol) == htons(ETH_P_TSN) )
             {
                 //this is a frame we want.
-                printf("THIS FRAME IS INTERESTING!!\n-----\n");
+                printf("TSN frame!\n-----\n");
                 struct ethernet_frame frame;
-                print_hex(frame.data, sizeof(struct timespec)); printf("\n");
+                // print_hex(frame.data, sizeof(struct timespec)); printf("\n");
                 memcpy(&frame, msg.msg_iov->iov_base, min(sizeof(frame), msg.msg_iov->iov_len));
                 memcpy(&time_from_source, frame.data+1, sizeof(struct timespec));
-                printf("Source sent at:  ");
-                print_timespec(time_from_source);
-                printf("\n");
                 if (get_hw_timestamp_from_msg(&msg, &time_from_nic))
                 {
-                    printf("NIC recevied at: ");
-                    print_timespec(time_from_nic);
-                    printf("\n");
-                    printf("Code received at: ");
-                    print_timespec(now);
-                    printf("\n");
                     memset(&t_prop, 0, sizeof(t_prop));
                     time_diff(&time_from_source, &time_from_nic, &t_prop);
-                    printf("Propagation time (NIC): ");
-                    print_timespec(t_prop);
                     t_prop.tv_sec -= MAX(get_num_leapseconds(), LEAP_SECONDS_OFFSET);
                     printf("Propagation time (NIC, corrected for UTC): ");
-                    print_timespec(t_prop);
-                    printf("\n");
-                    time_diff(&time_from_source, &now, &t_prop);
-                    printf("Propagation time (code): ");
                     print_timespec(t_prop);
                     printf("\n-----\n");
                     //TODO: add statistics and/or file-write
