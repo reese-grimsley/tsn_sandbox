b0d975888348da9ccf4a2b657e1ac2792c18a445
Reese <rgrimsle@andrew.cmu.edu>
2021-11-05 15:59:08 -0400

    Add ptp config and rule scripts (including NIC-to-sys sync)

--- ptp/ptp_client/run_ptp_client.sh
+++ ptp/ptp_client/run_ptp_client.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-if [[ `whoami` != "root" ]]; then
-    echo "Run as root/sudo!"; exit -1;
-fi
-
-echo "Start ptp..."
-sudo ptp4l -f ptp_client.conf -i enp87s0 -m &> ptp.log &
+sudo ptp4l -f ptp_client.conf -i enp87s0 -m >> ptp.log &
 echo "ptp4l is running in bg as pid $!"
 
-sleep 2
-
-echo "Start phc2sys..."
-sudo phc2sys -s enp87s0 -c CLOCK_REALTIME -w -m -O -37 &> phc2sys.log &  #37 seconds off between TAI and UTC as of Nov 11/21
+sudo phc2sys -s enp87s0 -c CLOCK_REALTIME -w -m >> phc2sys.log & 
 echo "phc2sys (sync NIC clock to sys clock) is running in bg as pid $!"
