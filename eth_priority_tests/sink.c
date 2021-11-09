3166d4bd5832d8a89e39baa094dc913cd9df269b
Reese <rgrimsle@andrew.cmu.edu>
2021-11-09 11:41:03 -0500

    add latency instrumentation

--- eth_priority_tests/sink.c
+++ eth_priority_tests/sink.c
@@ -243,11 +243,11 @@ void thread_recv_source_data()
                 printf("TSN frame!\n");
                 // print_hex(frame.data, sizeof(struct timespec)); printf("\n");
                 memcpy(&frame, msg.msg_iov->iov_base, min(sizeof(frame), msg.msg_iov->iov_len));
-                memcpy(&time_from_source, frame.payload.ss_payload.tx_time, sizeof(struct timespec));
+                memcpy(&time_from_source, frame.ss_payload.tx_time, sizeof(struct timespec));
 
-                frame_id = frame.payload.ss_payload.frame_id;
-                priority = frame.payload.ss_payload.priority;
-                test_id = frame.payload.ss_payload.test_id;
+                frame_id = frame.ss_payload.frame_id;
+                priority = frame.ss_payload.priority;
+                test_id = frame.ss_payload.test_id;
 
                 if (get_hw_timestamp_from_msg(&msg, &time_from_nic))
                 {
