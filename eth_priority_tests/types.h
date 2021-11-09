b9a60fff935e585ad4b869f048116c713a2cdfb0
Reese <rgrimsle@andrew.cmu.edu>
2021-11-09 18:01:24 -0500

    revert to manual priority setting; debug

--- eth_priority_tests/types.h
+++ eth_priority_tests/types.h
@@ -55,7 +55,6 @@ struct ethernet_RX_frame
     char source_mac[ETHER_ADDR_LEN];
     uint16_t data_size_or_type ; 
     uint16_t align_buffer;
-    uint32_t silly_frame_alignment;
     union eth_payload payload;
 };
 
