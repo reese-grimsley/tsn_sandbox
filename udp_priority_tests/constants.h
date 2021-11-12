65e336c947af7dbba5f6431490c2012384c239f7
Reese <rgrimsle@andrew.cmu.edu>
2021-11-12 17:07:35 -0500

    add VLAN IP and bind in source

--- udp_priority_tests/constants.h
+++ udp_priority_tests/constants.h
@@ -1,13 +1,16 @@
 #define SOURCE_NAME "tsn1"
 #define SOURCE_MAC_ADDR {0x54, 0xb2, 0x03, 0xf0, 0xd4, 0x55}  //54:b2:03:f0:d4:55
 #define SOURCE_IP_ADDR "192.168.1.229"
+#define SOURCE_IP_ADDR_VLAN "10.0.1.101"
 #define SINK_NAME 'tsn2'
 #define SINK_MAC_ADDR {0x54, 0xb2, 0x03, 0xf0, 0xd5, 0x0b}  //54:b2:03:f0:d5:0b
 #define SINK_IP_ADDR "192.168.1.231"
+#define SINK_IP_ADDR_VLAN "10.0.1.102"
 #define SINK_PORT 15810
 #define JAMMER_NAME "tsn3"
 #define JAMMER_MAC_ADDR {0x54, 0xb2, 0x03, 0xf0, 0xd4, 0x68}  //54:b2:03:f0:d4:68
 #define JAMMER_IP_ADDR  "192.168.1.224"
+#define JAMMER_IP_ADDR_VLAN "10.0.1.103"
 
 #define ETH_INTERFACE_I225 "enp87s0\0"
 #define ETH_INTERFACE_I225_VLAN3 "enp87s0.3\0"
