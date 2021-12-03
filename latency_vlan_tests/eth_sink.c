1ae5f7f370662c5a830a2bfc7e4ac61b256c98ea
Reese <rgrimsle@andrew.cmu.edu>
2021-12-03 11:26:11 -0500

    MIT license

--- latency_vlan_tests/eth_sink.c
+++ latency_vlan_tests/eth_sink.c
@@ -1,5 +1,26 @@
 /**
- * 
+MIT License
+
+Copyright (c) 2021 Reese Grimsley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
  * The sink will accept data from the jammer and source, although the test is if prioritized ethernet traffic (over VLAN)
  *     will have consistently lower latency than unprioritized traffic from the jammer.
  *  Expected behavior is that the prioritized traffic will not matter until the TSN switch is configured to given preference or particualr time slot
